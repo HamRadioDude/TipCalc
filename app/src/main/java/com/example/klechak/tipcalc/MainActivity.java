c5f80fee1f62852d76e48eb82002451b7c100828
sklechak1 <35785166+sklechak1@users.noreply.github.com>
2018-10-28 14:40:18 -0500

    Changes
    Corrected issue involving no input and crashed. Made the input type NumberDecimal for more accuracy.
    Added APK

--- app/src/main/java/com/example/klechak/tipcalc/MainActivity.java
+++ app/src/main/java/com/example/klechak/tipcalc/MainActivity.java
@@ -18,47 +18,75 @@ public class MainActivity extends AppCompatActivity {
     String tipChoice;
     double tipPerc;
     String fmt_totalTip;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText amount = (EditText)findViewById(R.id.txtCharges);
-        final EditText people = (EditText)findViewById(R.id.txtPpl);
-        final Spinner group =(Spinner)findViewById(R.id.txtGroup);
+        final EditText amount = (EditText) findViewById(R.id.txtCharges);
+        final EditText people = (EditText) findViewById(R.id.txtPpl);
+        final Spinner group = (Spinner) findViewById(R.id.txtGroup);
 
 
-        Button calc = (Button)findViewById(R.id.btnCalc);
-            calc.setOnClickListener(new View.OnClickListener(){
+        Button calc = (Button) findViewById(R.id.btnCalc);
+        calc.setOnClickListener(new View.OnClickListener() {
 
 
-                final TextView result = ((TextView)findViewById(R.id.txtTotal));
+                                    final TextView result = ((TextView) findViewById(R.id.txtTotal));
 
-                    public void onClick(View View) {
-                        billTotal = Double.parseDouble(amount.getText().toString());
-                        numPpl = Integer.parseInt(people.getText().toString());
-                        DecimalFormat currency = new DecimalFormat("$###,###.##");
+                                    public void onClick(View View) {
 
-                        tipChoice = group.getSelectedItem().toString();
-                        tipPerc = Double.parseDouble(tipChoice.replaceAll("[^0-9]",""));
+                                        // billTotal = Double.parseDouble(amount.getText().toString());
+//                            numPpl = Integer.parseInt(people.getText().toString());
+                                        float num = 0;
 
+                                        try {
+                                            billTotal = Double.parseDouble(amount.getText().toString());
+                                            numPpl = Integer.parseInt(people.getText().toString());
 
-
-                        totalTip = ((billTotal) *(tipPerc/100)) / numPpl ;
-
-                        fmt_totalTip = currency.format(totalTip);
-
-                        result.setText("Tip Amount Between " + numPpl + " people equals " + fmt_totalTip + " Each.");
-
-                    }
+                                            DecimalFormat currency = new DecimalFormat("$###,###.##");
 
 
+                                            tipChoice = group.getSelectedItem().toString();
+                                            tipPerc = Double.parseDouble(tipChoice.replaceAll("[^0-9]", ""));
 
 
-            }
+                                            totalTip = ((billTotal) * (tipPerc / 100)) / numPpl;
+
+                                            fmt_totalTip = currency.format(totalTip);
+
+                                            result.setText("Tip Amount Between " + numPpl + " people equals " + fmt_totalTip + " Each.");
 
 
+                                        } catch (NumberFormatException e) {
+                                            billTotal = 0;
+                                            numPpl = 0;
+                                            result.setText("THERE WAS A PROBLEM WITH YOUR INPUT");
+                                        }
 
-            );
+
+//  -------------- Klechak Note - I moved this into a try catch to prevent that horrible error from occuring (when someone forgets to put in input, program crashes.
+//                            DecimalFormat currency = new DecimalFormat("$###,###.##");
+//
+//
+//                        tipChoice = group.getSelectedItem().toString();
+//                        tipPerc = Double.parseDouble(tipChoice.replaceAll("[^0-9]",""));
+//
+//
+//
+//                        totalTip = ((billTotal) *(tipPerc/100)) / numPpl ;
+//
+//                        fmt_totalTip = currency.format(totalTip);
+//
+//                        result.setText("Tip Amount Between " + numPpl + " people equals " + fmt_totalTip + " Each.");
+
+                                    }
+
+
+                                }
+
+
+        );
     }
 }
